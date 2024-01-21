47425e7220d4bdc770ab08c24b42413af35826d3
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-01-22 01:17:20 +0800

    feat: matter physics init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { Point, PointCloud, Result, PDollarRecognizer } from "./pdollar.js";
 import { zhuyin } from "./charList.js";
 import * as PIXI from "pixi.js";
-import * as Matter from "matter-js";
+import Matter from "matter-js";
 
 let recognizer = new PDollarRecognizer();
 // store current stroke data
@@ -26,8 +26,8 @@ app.stage.hitArea = app.screen;
 // Load game assets
 const sheet = await PIXI.Assets.load("assets/sheet.json");
 const deer = PIXI.Sprite.from("assets/deer.svg");
-deer.x = 150;
-deer.y = 400;
+
+[deer.x, deer.y, deer.width, deer.height] = [100, 400, 200, 200]; 
 deer.anchor.set(0.5);
 deer.eventMode = "static";
 deer.cursor = "pointer";
@@ -55,10 +55,25 @@ deer.addEventListener("pointertap", () => {
 	loadTargetChar(zhuyin, charsContainer, resultChar);
 });
 
+const engine = Matter.Engine.create();
+const deerBody = Matter.Bodies.rectangle(deer.x, deer.y, deer.width, deer.height);
+const ground = Matter.Bodies.rectangle(0, 800, window.innerWidth, window.innerHeight, { isStatic: true });
+const mouseConstraint = Matter.MouseConstraint.create(engine, {
+	element: app.view
+});
+
+Matter.Composite.add(engine.world, [deerBody, ground, mouseConstraint]);
+
+// Constantly update screen
+app.ticker.add((delta) => {
+	deer.x = deerBody.position.x;
+	deer.y = deerBody.position.y;
+	Matter.Engine.update(engine, delta * 1000 / 60);
+});
+
 function startStroke(event) {
 	drawData.isDrawing = true;
-	drawData.coord.x = event.globalX;
-	drawData.coord.y = event.globalY;
+	[drawData.coord.x, drawData.coord.y] = [event.globalX, event.globalY];
 	handDrawnArea.lineStyle({ width: 5, color: "blue", cap: "round" });
 	drawData.currentPoints.push(new Point(Math.round(drawData.coord.x), Math.round(drawData.coord.y), ++drawData.strokeId));
 }
@@ -83,10 +98,13 @@ function recognizeStroke(points) {
 	// Signal to begin new gesture on next mouse/touchdown
 	drawData.strokeId = 0;
 	drawData.currentPoints = [];
+	if(result.Score < 0.5) return;
 	return result.Name;
 }
 
 async function loadTargetChar(framesArr, container, target) {
+	handDrawnArea.clear();
+	if (!target) return;
 	const foundChar = framesArr.find((char) => {
 		return char.slice(0, char.length - 4) === target;
 	});
@@ -94,5 +112,12 @@ async function loadTargetChar(framesArr, container, target) {
 	charSprite.x = Math.floor(Math.random() * (window.innerWidth)) + 1;
 	charSprite.y = Math.floor(Math.random() * (window.innerHeight)) + 1;
 	container.addChild(charSprite);
-	handDrawnArea.clear();
+	// Create body for char sprite
+	const charBody = Matter.Bodies.rectangle(charSprite.x, charSprite.y, charSprite.width, charSprite.height);
+	Matter.Composite.add(engine.world, charBody);
+	app.ticker.add((delta) => {
+		charSprite.x = charBody.position.x;
+		charSprite.y = charBody.position.y;
+		Matter.Engine.update(engine, delta * 1000 / 60);
+	});
 }
